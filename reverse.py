4d4599569435414c8ac385c7c58d1f2e070d7138
Brandon Tweed <btweed@broadvine.com>
2017-10-12 09:58:14 -0400

    Fix it real good

--- reverse.py
+++ reverse.py
@@ -1,14 +1,14 @@
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     to_reverse = 'reverse'
     work = []
     for c in to_reverse:
-       work.push(c)
-    
+       work.insert(0, c)
+
     result = []
     while len(work) > 0:
-        result.add(work.pop())
-        
+        result.insert(0, work.pop())
+
     print(str(result))
-    
+
