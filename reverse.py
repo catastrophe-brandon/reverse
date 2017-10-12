6ddb0d92805b959b6e00d35d82cab58ef70db442
Brandon Tweed <btweed@broadvine.com>
2017-10-12 10:03:43 -0400

    Properly convert the list to a string

--- reverse.py
+++ reverse.py
@@ -1,14 +1,19 @@
 
-
-if __name__ == '__main__':
+def stack_based_reverse(some_str):
     to_reverse = 'reverse'
     work = []
     for c in to_reverse:
-       work.insert(0, c)
+        work.insert(0, c)
 
     result = []
     while len(work) > 0:
         result.insert(0, work.pop())
 
-    print(str(result))
+    return ''.join(result)
+
+
+if __name__ == '__main__':
+    to_reverse = 'reverse'
+    print('String to reverse: {}'.format(to_reverse))
+    print(stack_based_reverse(to_reverse))
 
